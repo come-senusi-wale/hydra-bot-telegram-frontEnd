7e2f2b57f57e4c928305567be1a7a8eb9da1794c
come-senusi-wale <akinyemisaheedwale@gmailcom>
2024-08-08 14:02:48 +0100

    url

--- api/wallet.ts
+++ api/wallet.ts
@@ -2,7 +2,7 @@
 
 // export const baseUrl = 'http://localhost:8081/api/v1'
 // export const baseUrl = 'https://snipper-bot-ancient-dew-8766-dawn-cloud-8655.fly.dev/api/v1'
-export const baseUrl = 'https://consumer-snipper-bot.fly.dev/'
+export const baseUrl = 'https://consumer-snipper-bot.fly.dev/api/v1'
 
 // export const createWallet = (body: any) => {
 //     return axiosInstance().post("/create_account", body);
